08bbe9a5c65486c6531341a5674a6cc7b8a93141
leticia-rsm <leticia.mendes@sptech.school>
2025-05-29 16:46:45 -0300

    Script e modelagem atualizados

--- scriptInovaairTabelas.sql
+++ scriptInovaairTabelas.sql
@@ -1,4 +1,4 @@
-# DROP DATABASE innovaair;
+-- DROP DATABASE innovaair;
 
 CREATE DATABASE IF NOT EXISTS innovaair;
 USE innovaair;
@@ -92,6 +92,18 @@ CREATE TABLE IF NOT EXISTS captura_alerta (
   CONSTRAINT fk_alerta_metrica FOREIGN KEY (fkMetrica) REFERENCES metrica (idMetrica)
 );
 
+CREATE TABLE IF NOT EXISTS dados_previsao (
+  idPrevisao INT PRIMARY KEY AUTO_INCREMENT,
+  valorPrevisto FLOAT NOT NULL,
+  isPrevisao TINYINT NOT NULL,
+  gravidade VARCHAR(20) NOT NULL,
+  momento DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fkMetrica INT NOT NULL,
+  CONSTRAINT fk_previs_metrica FOREIGN KEY (fkMetrica) REFERENCES metrica (idMetrica)
+);
+
+SELECT * FROM dados_previsao;
+
 INSERT INTO cliente (razaoSocial, cnpj, email, telefone) VALUES
 ('InnovaAir', '12345678000188', 'inovaair@technology.com', '1133224455'),
 ('TAM LINHAS AÉREAS S.A. A LATAM', '12345678000188', 'contato@latam.com.br', '1133224455');
@@ -125,6 +137,71 @@ INSERT INTO filial (terminal, setor, fkCliente, fkEndereco) VALUES
 INSERT INTO usuarioFilial VALUES
 (2,1),
 (2,2);
+
+INSERT INTO maquina (fkFilial, numeroSerial, enderecoMac, hostname) VALUES
+(1, 'SN1001', '00:1A:2B:3C:4D:5E', 'maquina-1'),
+(2, 'SN1002', '00:1A:2B:3C:4D:5F', 'maquina-2'),
+(1, 'SN1003', '00:1A:2B:3C:4D:60', 'maquina-3'),
+(2, 'SN1004', '00:1A:2B:3C:4D:61', 'maquina-4'),
+(1, 'SN1005', '00:1A:2B:3C:4D:62', 'maquina-5'),
+(2, 'SN1006', '00:1A:2B:3C:4D:63', 'maquina-6'),
+(1, 'SN1007', '00:1A:2B:3C:4D:64', 'maquina-7'),
+(2, 'SN1008', '00:1A:2B:3C:4D:65', 'maquina-8'),
+(1, 'SN1009', '00:1A:2B:3C:4D:66', 'maquina-9'),
+(2, 'SN1010', '00:1A:2B:3C:4D:67', 'maquina-10');
+
+-- Inserir componentes (4 por máquina)
+INSERT INTO componente (fkMaquina, componente, especificacao) VALUES
+(1, 'Processador', 'Ryzen 3'), (1, 'RAM', 'Kingston 16GB'), (1, 'Armazenamento', 'Samsung SSD 1TB'), (1, 'Rede', 'Intel Gigabit'),
+(2, 'Processador', 'Intel i5'), (2, 'RAM', 'Corsair 32GB'), (2, 'Armazenamento', 'WD Blue 2TB'), (2, 'Rede', 'Realtek 1G'),
+(3, 'Processador', 'Intel i7'), (3, 'RAM', 'HyperX 8GB'), (3, 'Armazenamento', 'Crucial 1TB'), (3, 'Rede', 'TP-Link 1G'),
+(4, 'Processador', 'Ryzen 5'), (4, 'RAM', 'G.Skill 16GB'), (4, 'Armazenamento', 'Seagate 2TB'), (4, 'Rede', 'D-Link 1G'),
+(5, 'Processador', 'Intel Xeon'), (5, 'RAM', 'Patriot 8GB'), (5, 'Armazenamento', 'Kingston SSD 500GB'), (5, 'Rede', 'Intel Ethernet'),
+(6, 'Processador', 'Ryzen 7'), (6, 'RAM', 'Corsair 16GB'), (6, 'Armazenamento', 'Samsung SSD 2TB'), (6, 'Rede', 'Realtek 2.5G'),
+(7, 'Processador', 'Intel i9'), (7, 'RAM', 'Kingston 32GB'), (7, 'Armazenamento', 'WD Black 1TB'), (7, 'Rede', 'TP-Link 1G'),
+(8, 'Processador', 'Ryzen 9'), (8, 'RAM', 'G.Skill 8GB'), (8, 'Armazenamento', 'Crucial SSD 2TB'), (8, 'Rede', 'D-Link 2.5G'),
+(9, 'Processador', 'Intel Xeon'), (9, 'RAM', 'HyperX 16GB'), (9, 'Armazenamento', 'Seagate 1TB'), (9, 'Rede', 'Intel Gigabit'),
+(10, 'Processador', 'Ryzen 5'), (10, 'RAM', 'Corsair 16GB'), (10, 'Armazenamento', 'Kingston 512GB'), (10, 'Rede', 'Intel Ethernet');
+
+-- Inserir métricas para cada componente (supondo ids de componente consecutivos)
+-- RAM: porcentagemUso
+INSERT INTO metrica (metrica, limiteMinimo, limiteMaximo, fkComponente) VALUES
+('porcentagemUso', 20, 90, 2), ('porcentagemUso', 25, 85, 6), ('porcentagemUso', 30, 80, 10), ('porcentagemUso', 20, 95, 14),
+('porcentagemUso', 15, 75, 18), ('porcentagemUso', 25, 85, 22), ('porcentagemUso', 30, 80, 26), ('porcentagemUso', 20, 95, 30),
+('porcentagemUso', 15, 75, 34), ('porcentagemUso', 25, 85, 38);
+
+-- Processador: porcentagemUso, processos, tempoAtividade
+INSERT INTO metrica (metrica, limiteMinimo, limiteMaximo, fkComponente) VALUES
+('porcentagemUso', 30, 95, 1), ('processos', 500, 4000, 1), ('tempoAtividade', NULL, NULL, 1),
+('porcentagemUso', 35, 90, 5), ('processos', 400, 3000, 5), ('tempoAtividade', NULL, NULL, 5),
+('porcentagemUso', 25, 85, 9), ('processos', 200, 2000, 9), ('tempoAtividade', NULL, NULL, 9),
+('porcentagemUso', 20, 80, 13), ('processos', 600, 3500, 13), ('tempoAtividade', NULL, NULL, 13),
+('porcentagemUso', 30, 95, 17), ('processos', 500, 4000, 17), ('tempoAtividade', NULL, NULL, 17),
+('porcentagemUso', 25, 85, 21), ('processos', 300, 2500, 21), ('tempoAtividade', NULL, NULL, 21),
+('porcentagemUso', 35, 90, 25), ('processos', 450, 3500, 25), ('tempoAtividade', NULL, NULL, 25),
+('porcentagemUso', 20, 80, 29), ('processos', 500, 4000, 29), ('tempoAtividade', NULL, NULL, 29),
+('porcentagemUso', 30, 95, 33), ('processos', 350, 3000, 33), ('tempoAtividade', NULL, NULL, 33),
+('porcentagemUso', 35, 90, 37), ('processos', 600, 4000, 37), ('tempoAtividade', NULL, NULL, 37);
+
+-- Armazenamento: porcentagemUso
+INSERT INTO metrica (metrica, limiteMinimo, limiteMaximo, fkComponente) VALUES
+('porcentagemUso', 40, 95, 3), ('porcentagemUso', 30, 85, 7), ('porcentagemUso', 35, 90, 11), ('porcentagemUso', 25, 75, 15),
+('porcentagemUso', 30, 85, 19), ('porcentagemUso', 35, 90, 23), ('porcentagemUso', 25, 75, 27), ('porcentagemUso', 30, 85, 31),
+('porcentagemUso', 35, 90, 35), ('porcentagemUso', 25, 75, 39);
+
+-- Rede: velocidadeUpload, velocidadeDownload
+INSERT INTO metrica (metrica, limiteMinimo, limiteMaximo, fkComponente) VALUES
+('velocidadeUpload', 10, 100, 4), ('velocidadeDownload', 20, 200, 4),
+('velocidadeUpload', 15, 150, 8), ('velocidadeDownload', 25, 250, 8),
+('velocidadeUpload', 20, 120, 12), ('velocidadeDownload', 30, 220, 12),
+('velocidadeUpload', 10, 110, 16), ('velocidadeDownload', 20, 210, 16),
+('velocidadeUpload', 15, 140, 20), ('velocidadeDownload', 25, 240, 20),
+('velocidadeUpload', 20, 130, 24), ('velocidadeDownload', 30, 230, 24),
+('velocidadeUpload', 10, 105, 28), ('velocidadeDownload', 20, 205, 28),
+('velocidadeUpload', 15, 145, 32), ('velocidadeDownload', 25, 245, 32),
+('velocidadeUpload', 20, 135, 36), ('velocidadeDownload', 30, 235, 36),
+('velocidadeUpload', 10, 115, 40), ('velocidadeDownload', 20, 215, 40);
+
 
 CREATE VIEW dashRobertoModelos as
 SELECT
@@ -174,3 +251,6 @@ GROUP BY
     todas_combinacoes.componente,
     semanas,
 	idUsuario;
+    
+-- SELECT LETÍCIA
+select valorPrevisto, isPrevisao, gravidade, momento, componente from dados_previsao join metrica on fkMetrica = idMetrica join componente on fkComponente = idComponente limit 35040;
